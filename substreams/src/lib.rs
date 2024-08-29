15823262d558d8f34342ae0654e6a5fa0c84b964
tarunsharma <tarun.kr.sharma@hotmail.com>
2024-08-29 12:30:07 +0400

    feat: added new filter instruction module for vault stratagy program

--- substreams/src/lib.rs
+++ substreams/src/lib.rs
@@ -2,6 +2,7 @@ mod pb;
 mod map_block_full;
 mod map_block_meta;
 mod map_filter_instructions;
+mod map_filter_vault_strategy_instructions;
 mod map_filter_transactions;
 mod map_block_without_votes;
 mod map_parse_journal_data;
