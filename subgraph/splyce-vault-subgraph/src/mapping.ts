4f29d57a4ca3ab3d47fd2d08450812ae1c0e67fe
tarunsharma <tarun.kr.sharma@hotmail.com>
2024-09-10 18:46:21 +0400

    feat: added subgraph to index vault init with token as child entity

--- subgraph/splyce-vault-subgraph/src/mapping.ts
+++ subgraph/splyce-vault-subgraph/src/mapping.ts
@@ -1,28 +1,38 @@
-import { log } from "@graphprotocol/graph-ts";
+import { BigDecimal, log, BigInt } from "@graphprotocol/graph-ts";
 import { Protobuf } from 'as-proto/assembly';
-import { JournalEntry } from "./pb/sol/transactions/journal/v1/JournalEntry";
-import { JournalEntity } from "../generated/schema";
-import { Journals } from "./pb/sol/transactions/journal/v1/Journals";
+import { VaultEvent } from "./pb/vault/events/v1/VaultEvent";
+import {Token, Vault} from "../generated/schema";
+import { VaultInitEvent } from "./pb/vault/events/v1/VaultInitEvent";
 
 export function handleTransactions(bytes: Uint8Array): void {
+    const vaultEvent: VaultEvent = Protobuf.decode<VaultEvent>(bytes, VaultEvent.decode);
+    if (vaultEvent.vaultInitialize !== null) {
+        log.info("VaultInitialize: {0}", [vaultEvent.vaultInitialize!.vaultIndex.toString()]);
 
-    const journalsProto: Journals = Protobuf.decode<Journals>(bytes, Journals.decode);
-    const journals = journalsProto.journals;
+        let vault = new Vault(vaultEvent.vaultInitialize!.vaultIndex.toString());
+        vault.depositLimit = BigInt.fromU64(vaultEvent.vaultInitialize!.depositLimit);
+        vault.shutdown = false;
+        vault.totalDebt =    BigInt.fromI32(0);
+        vault.totalIdle = BigInt.fromI32(0);
+        vault.apr  =   BigDecimal.fromString("0");
+        
+        //Create token entity
+        vault.token  =  getOrCreateTokenEntity(vaultEvent.vaultInitialize!).id;
 
-    if (journals.length == 0) {
-        log.info("No journal found", []);
-        return;
+        vault.save();
     }
 
-    for (let i = 0; i < journals.length; i++) {
-        let journal = journals[i];
+}
 
-        log.info("***journal found*** {0}", [journal.title]);
-
-        let entity = new JournalEntity(journal.id);
-        entity.title = journal.title;
-        entity.message = journal.message;
-        entity.save();
+function getOrCreateTokenEntity(vaultInitEvent: VaultInitEvent): Token {
+    let token = Token.load(vaultInitEvent.underlyingMint.toString());
+    if (token == null) {
+        token = new Token(vaultInitEvent.underlyingMint.toString());
+        token.decimals = vaultInitEvent.underlyingDecimals;
+        token.symbol = ""; //TODO: Get symbol from mint
+        token.name = "";   //TODO: Get name from mint 
+        token.save();
     }
 
+    return token as Token;
 }
